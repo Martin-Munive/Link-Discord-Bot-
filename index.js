225f8d76138efb7b6a6d0046599fd0bd5f9f96db
Martin-Munive <nestor.munive.a@gmail.com>
2022-06-27 17:34:14 -0500

    Archivos de inicio. Index vacío.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Links_Bot.
+// Es un Bot de propósito general, para las necesitades de los usuarios de el grupo ThePros.
+// Su propósito principal es ordenar y administrar los elementos que se comparten en el chat.
+// Tiene un set de funciones personalizadas:
+// FUNCIONES:
+// 1.(P) Da un formato a los links publicados en Discord. Los categoriza y pone palabras clave.
+// 2.(P) Verificar los canales para evaluar lo publicado y comentar en el canal general la Metadata.
+// 3.(P) Hacer inventario de lo que se ha publicado, en el momento, en el día de la consulta.
